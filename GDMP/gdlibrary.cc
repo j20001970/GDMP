9147d363bf69a279dc7bfb37c4550366355854f3
Jason Kuo <j20001970@gmail.com>
2021-12-08 21:22:04 +0800

    add NO_GDMP define to exclude GDMP itself

--- GDMP/gdlibrary.cc
+++ GDMP/gdlibrary.cc
@@ -1,4 +1,6 @@
+#ifndef NO_GDMP
 #include "gdmp.h"
+#endif
 
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
     godot::Godot::gdnative_init(o);
@@ -10,6 +12,7 @@ extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_opt
 
 extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
     godot::Godot::nativescript_init(handle);
-
+    #ifndef NO_GDMP
     godot::register_class<godot::GDMP>();
+    #endif
 }
