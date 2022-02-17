ab67e91c7d424ec63558f31b399e95eda5bccd49
Jason Kuo <j20001970@gmail.com>
2022-02-18 02:40:00 +0800

    update proto nativescript location

--- addons/GDMP/GDMP.gd
+++ addons/GDMP/GDMP.gd
@@ -6,36 +6,36 @@ enum {
 }
 
 # Classification
-const Classification : NativeScript = preload("Classification.gdns")
-const ClassificationList : NativeScript = preload("ClassificationList.gdns")
-const ClassificationListCollection : NativeScript = preload("ClassificationListCollection.gdns")
+const Classification : NativeScript = preload("proto/Classification.gdns")
+const ClassificationList : NativeScript = preload("proto/ClassificationList.gdns")
+const ClassificationListCollection : NativeScript = preload("proto/ClassificationListCollection.gdns")
 
 # Detection
-const Detection : NativeScript = preload("Detection.gdns")
-const DetectionList : NativeScript = preload("DetectionList.gdns")
+const Detection : NativeScript = preload("proto/Detection.gdns")
+const DetectionList : NativeScript = preload("proto/DetectionList.gdns")
 
 # Landmark
-const Landmark : NativeScript = preload("Landmark.gdns")
-const LandmarkList : NativeScript = preload("LandmarkList.gdns")
-const LandmarkListCollection : NativeScript = preload("LandmarkListCollection.gdns")
-const NormalizedLandmark : NativeScript = preload("NormalizedLandmark.gdns")
-const NormalizedLandmarkList : NativeScript = preload("NormalizedLandmarkList.gdns")
-const NormalizedLandmarkListCollection : NativeScript = preload("NormalizedLandmarkListCollection.gdns")
+const Landmark : NativeScript = preload("proto/Landmark.gdns")
+const LandmarkList : NativeScript = preload("proto/LandmarkList.gdns")
+const LandmarkListCollection : NativeScript = preload("proto/LandmarkListCollection.gdns")
+const NormalizedLandmark : NativeScript = preload("proto/NormalizedLandmark.gdns")
+const NormalizedLandmarkList : NativeScript = preload("proto/NormalizedLandmarkList.gdns")
+const NormalizedLandmarkListCollection : NativeScript = preload("proto/NormalizedLandmarkListCollection.gdns")
 
 # LocationData
-const BoundingBox : NativeScript = preload("BoundingBox.gdns")
-const RelativeBoundingBox : NativeScript = preload("RelativeBoundingBox.gdns")
-const BinaryMask : NativeScript = preload("BinaryMask.gdns")
-const RelativeKeypoint : NativeScript = preload("RelativeKeypoint.gdns")
-const LocationData : NativeScript = preload("LocationData.gdns")
+const BoundingBox : NativeScript = preload("proto/BoundingBox.gdns")
+const RelativeBoundingBox : NativeScript = preload("proto/RelativeBoundingBox.gdns")
+const BinaryMask : NativeScript = preload("proto/BinaryMask.gdns")
+const RelativeKeypoint : NativeScript = preload("proto/RelativeKeypoint.gdns")
+const LocationData : NativeScript = preload("proto/LocationData.gdns")
 
 # Rasterization
-const Interval : NativeScript = preload("Interval.gdns")
-const Rasterization : NativeScript = preload("Rasterization.gdns")
+const Interval : NativeScript = preload("proto/Interval.gdns")
+const Rasterization : NativeScript = preload("proto/Rasterization.gdns")
 
 # Rect
-const Rect : NativeScript = preload("Rect.gdns")
-const NormalizedRect : NativeScript = preload("NormalizedRect.gdns")
+const Rect : NativeScript = preload("proto/Rect.gdns")
+const NormalizedRect : NativeScript = preload("proto/NormalizedRect.gdns")
 
 var plugin : Object
 var packet_data : Dictionary = {}
