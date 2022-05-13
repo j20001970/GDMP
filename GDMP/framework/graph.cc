1ec580f0b576a0a4e6f81480db545e709aaaaf2a
Jason Kuo <j20001970@gmail.com>
2022-05-13 17:45:11 +0800

    use GetResourceContents to load graph files

--- GDMP/framework/graph.cc
+++ GDMP/framework/graph.cc
@@ -10,6 +10,7 @@
 #include "mediapipe/framework/packet.h"
 #include "mediapipe/framework/port/parse_text_proto.h"
 #include "mediapipe/framework/port/status.h"
+#include "mediapipe/util/resource_util.h"
 
 using namespace godot;
 
@@ -31,20 +32,15 @@ void Graph::_init() {
 void Graph::initialize(String graph_path, bool as_text) {
 	absl::Status result = [this, &graph_path, &as_text]() -> absl::Status {
 		graph = std::make_unique<mediapipe::CalculatorGraph>();
-		Ref<File> file = Ref<File>(File::_new());
-		Error err;
-		err = file->open(graph_path, File::READ);
-		if (err != Error::OK) {
-			return absl::UnknownError(String("Failed to open graph file: {0}").format(Array::make((int)err)).alloc_c_string());
-		}
+		std::string graph_contents;
+		MP_RETURN_IF_ERROR(mediapipe::GetResourceContents(graph_path.alloc_c_string(), &graph_contents, !as_text));
 		mediapipe::CalculatorGraphConfig config;
 		bool result;
 		if (as_text) {
-			result = mediapipe::ParseTextProto<mediapipe::CalculatorGraphConfig>(file->get_as_text().alloc_c_string(), &config);
+			result = mediapipe::ParseTextProto(graph_contents, &config);
 		} else {
-			result = config.ParseFromArray(file->get_buffer(file->get_len()).read().ptr(), file->get_len());
+			result = config.ParseFromArray(graph_contents.data(), graph_contents.size());
 		}
-		file->close();
 		if (!result) {
 			return absl::InvalidArgumentError("Failed to parse graph config.");
 		}
