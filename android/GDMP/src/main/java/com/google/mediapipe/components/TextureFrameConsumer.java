0413e3a73189fa7e9363fb5b5a281da18eec1dab
Jason Kuo <j20001970@gmail.com>
2023-04-23 12:27:31 +0800

    simply android project structure

--- android/GDMP/src/main/java/com/google/mediapipe/components/TextureFrameConsumer.java
+++
@@ -1,1 +0,0 @@
-../../../../../../../../../mediapipe/mediapipe/java/com/google/mediapipe/components/TextureFrameConsumer.java
---
+++ android/src/main/java/com/google/mediapipe/components/ExternalTextureConverter.java
@@ -0,0 +1,1 @@
+../../../../../../../../mediapipe/mediapipe/java/com/google/mediapipe/components/ExternalTextureConverter.java
