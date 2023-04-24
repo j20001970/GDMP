1be5ea0ab35c4db7ffef5e8910fc4120b339e1be
Jason Kuo <j20001970@gmail.com>
2023-04-25 00:46:37 +0800

    use junction directory for windows and other improvements

--- setup.py
+++ setup.py
@@ -1,39 +1,47 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
-import os
+from os import path, symlink, unlink
 import subprocess
 import sys
-from shutil import copy, copytree, rmtree, which
+from shutil import rmtree, which
 
-mediapipe_dir = os.path.join(os.path.dirname(__file__), 'mediapipe')
+mediapipe_dir = path.join(path.dirname(__file__), 'mediapipe')
 
 # Patching mediapipe workspace
-mediapipe_setup = os.path.join(os.path.dirname(__file__), 'mediapipe_setup.diff')
+mediapipe_setup = path.join(path.dirname(__file__), 'mediapipe_setup.diff')
 if which('git'):
     print("Using git to patch mediapipe workspace.")
-    subprocess.run(['git', 'apply', '--unsafe-paths', '--directory='+mediapipe_dir, mediapipe_setup])
+    subprocess.run(['git', 'apply', '--unsafe-paths',
+                   '--directory='+mediapipe_dir, mediapipe_setup])
 elif which('patch'):
     print("Using patch to patch mediapipe workspace.")
-    subprocess.run(['patch', '-p1', '-d', mediapipe_dir, '-i', mediapipe_setup])
+    subprocess.run(
+        ['patch', '-p1', '-d', mediapipe_dir, '-i', mediapipe_setup])
 else:
-    print("'git' or 'patch' cannot be found for patching mediapipe workspace.")
+    print("Error: 'git' or 'patch' cannot be found for patching mediapipe workspace.")
     sys.exit(-1)
 
-# Symlink or copy GDMP source code to mediapipe workspace
+# Symlink GDMP source code to mediapipe workspace
+src_dir = path.join(path.dirname(__file__), 'GDMP')
+dst_dir = path.join(mediapipe_dir, 'mediapipe', 'GDMP')
+if path.isdir(dst_dir):
+    try:
+        unlink(dst_dir)
+    except:
+        rmtree(dst_dir)
 try:
-    copy_dir = os.path.join(os.path.dirname(__file__), 'GDMP')
-    dest_dir = os.path.join(mediapipe_dir, 'mediapipe', 'GDMP')
-    if not os.path.isdir(dest_dir):
-        os.symlink(copy_dir, dest_dir, True)
-except OSError:
-    print("Symlink not available, procceed to copy files.")
-    if os.path.isdir(dest_dir):
-        rmtree(dest_dir)
-    copytree(copy_dir, dest_dir)
+    if sys.platform.startswith('win32'):
+        subprocess.check_call('mklink /J "%s" "%s"' %
+                              (dst_dir, src_dir), shell=True)
+    else:
+        symlink(src_dir, dst_dir, True)
+except:
+    print("Error: Unable to symlink GDMP source to mediapipe workspace.")
+    sys.exit(-1)
 
 # Symlink external dependencies
 try:
-    external_dir = os.path.join(mediapipe_dir, "external")
-    os.symlink("bazel-out/../../../external", external_dir, True)
-except Exception:
+    external_dir = path.join(mediapipe_dir, "external")
+    symlink("bazel-out/../../../external", external_dir, True)
+except:
     print("Failed to symlink external dependencies, skipping.")
