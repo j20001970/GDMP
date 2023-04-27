08c16775eafb3e68a728ad6242f05afbb6b58c7e
Jason Kuo <j20001970@gmail.com>
2023-04-28 01:43:00 +0800

    build and setup script improvements

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-from os import path, symlink, unlink
-import subprocess
 import sys
+from os import path, symlink, unlink
 from shutil import rmtree, which
+from subprocess import run
 
 mediapipe_dir = path.join(path.dirname(__file__), 'mediapipe')
 
@@ -11,12 +11,11 @@ mediapipe_dir = path.join(path.dirname(__file__), 'mediapipe')
 mediapipe_setup = path.join(path.dirname(__file__), 'mediapipe_setup.diff')
 if which('git'):
     print("Using git to patch mediapipe workspace.")
-    subprocess.run(['git', 'apply', '--unsafe-paths',
-                   '--directory='+mediapipe_dir, mediapipe_setup])
+    run(['git', 'apply', '--unsafe-paths',
+        '--directory='+mediapipe_dir, mediapipe_setup])
 elif which('patch'):
     print("Using patch to patch mediapipe workspace.")
-    subprocess.run(
-        ['patch', '-p1', '-d', mediapipe_dir, '-i', mediapipe_setup])
+    run(['patch', '-p1', '-d', mediapipe_dir, '-i', mediapipe_setup])
 else:
     print("Error: 'git' or 'patch' cannot be found for patching mediapipe workspace.")
     sys.exit(-1)
@@ -31,8 +30,7 @@ if path.isdir(dst_dir):
         rmtree(dst_dir)
 try:
     if sys.platform.startswith('win32'):
-        subprocess.check_call('mklink /J "%s" "%s"' %
-                              (dst_dir, src_dir), shell=True)
+        run('mklink /J "%s" "%s"' % (dst_dir, src_dir), check=True, shell=True)
     else:
         symlink(src_dir, dst_dir, True)
 except:
