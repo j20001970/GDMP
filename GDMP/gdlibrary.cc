e1a96f4ba9f68e2ea8ebbc62c98538ed3586f9e8
Jason Kuo <j20001970@gmail.com>
2023-05-18 08:30:30 +0800

    retire legacy proto classes

--- GDMP/gdlibrary.cc
+++ GDMP/gdlibrary.cc
@@ -10,11 +10,6 @@
 #include "GDMP/gpu/gpu_helper.h"
 #include "GDMP/gpu/gpu_resources.h"
 #include "GDMP/io/camera_helper.h"
-#include "GDMP/proto/classification.h"
-#include "GDMP/proto/detection.h"
-#include "GDMP/proto/landmark.h"
-#include "GDMP/proto/location_data.h"
-#include "GDMP/proto/rect.h"
 #include "GDMP/proto/proto.h"
 #include "GDMP/util/resource_util.h"
 
@@ -41,33 +36,6 @@ extern "C" void GDN_EXPORT mediapipe_nativescript_init(void *handle) {
 	register_class<MediaPipeGPUResources>();
 	// I/O
 	register_class<MediaPipeCameraHelper>();
-	// Classification
-	register_class<Classification>();
-	register_class<ClassificationList>();
-	register_class<ClassificationListCollection>();
-	// Deteciton
-	register_class<AssociatedDetection>();
-	register_class<Detection>();
-	register_class<DetectionList>();
-	// Landmark
-	register_class<Landmark>();
-	register_class<LandmarkList>();
-	register_class<LandmarkListCollection>();
-	register_class<NormalizedLandmark>();
-	register_class<NormalizedLandmarkList>();
-	register_class<NormalizedLandmarkListCollection>();
-	// LocationData
-	register_class<BoundingBox>();
-	register_class<RelativeBoundingBox>();
-	register_class<BinaryMask>();
-	register_class<RelativeKeypoint>();
-	register_class<LocationData>();
-	// Rasterization
-	register_class<Interval>();
-	register_class<Rasterization>();
-	// Rect
-	register_class<Rect>();
-	register_class<NormalizedRect>();
 	// ResourceProvider
 	set_resource_provider();
 }
