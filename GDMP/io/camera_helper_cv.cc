5383de05f19390f3dabb82807e4c9474183bd4b5
Jason Kuo <j20001970@gmail.com>
2023-05-22 23:31:41 +0800

    camera thread member function

--- GDMP/io/camera_helper_cv.cc
+++ GDMP/io/camera_helper_cv.cc
@@ -22,6 +22,39 @@ class CameraHelperCV : public CameraHelperImpl, public cv::VideoCapture {
 #endif
 		std::thread thread;
 
+		void camera_thread() {
+			auto cv_format = cv::COLOR_BGR2RGB;
+				auto image_format = mediapipe::ImageFormat::SRGB;
+#if !MEDIAPIPE_DISABLE_GPU
+				Ref<MediaPipeGPUHelper> gpu_helper;
+				if (gpu_resources != nullptr) {
+					cv_format = cv::COLOR_BGR2RGBA;
+					image_format = mediapipe::ImageFormat::SRGBA;
+					gpu_helper = Ref(MediaPipeGPUHelper::_new(gpu_resources.get()));
+				}
+#endif
+				grab_frames = true;
+				while (grab_frames) {
+					cv::Mat video_frame;
+					*this >> video_frame;
+					if (flip) {
+						cv::flip(video_frame, video_frame, 1);
+					}
+					cv::cvtColor(video_frame, video_frame, cv_format);
+					auto input_frame = std::make_shared<mediapipe::ImageFrame>(
+							image_format, video_frame.cols, video_frame.rows,
+							mediapipe::ImageFrame::kGlDefaultAlignmentBoundary);
+					cv::Mat input_frame_mat = mediapipe::formats::MatView(input_frame.get());
+					video_frame.copyTo(input_frame_mat);
+					Ref<MediaPipeImage> image = MediaPipeImage::_new(input_frame);
+#if !MEDIAPIPE_DISABLE_GPU
+					if (gpu_helper.is_valid())
+						image = gpu_helper->make_gpu_image(image);
+#endif
+					camera_helper->emit_signal("new_frame", image);
+				}
+		}
+
 	public:
 		CameraHelperCV(MediaPipeCameraHelper *camera_helper) :
 				CameraHelperImpl(camera_helper) {
@@ -52,42 +85,10 @@ class CameraHelperCV : public CameraHelperImpl, public cv::VideoCapture {
 
 		void start(int index, Vector2 size) {
 			open(index);
-			ERR_FAIL_COND(!isOpened());
 			set(cv::CAP_PROP_FRAME_WIDTH, size.x);
 			set(cv::CAP_PROP_FRAME_HEIGHT, size.y);
 			ERR_FAIL_COND(!isOpened());
-			thread = std::thread([this, size]() -> void {
-				auto cv_format = cv::COLOR_BGR2RGB;
-				auto image_format = mediapipe::ImageFormat::SRGB;
-#if !MEDIAPIPE_DISABLE_GPU
-				Ref<MediaPipeGPUHelper> gpu_helper;
-				if (gpu_resources != nullptr) {
-					cv_format = cv::COLOR_BGR2RGBA;
-					image_format = mediapipe::ImageFormat::SRGBA;
-					gpu_helper = Ref(MediaPipeGPUHelper::_new(gpu_resources.get()));
-				}
-#endif
-				grab_frames = true;
-				while (grab_frames) {
-					cv::Mat video_frame;
-					*this >> video_frame;
-					if (flip) {
-						cv::flip(video_frame, video_frame, 1);
-					}
-					cv::cvtColor(video_frame, video_frame, cv_format);
-					auto input_frame = std::make_shared<mediapipe::ImageFrame>(
-							image_format, video_frame.cols, video_frame.rows,
-							mediapipe::ImageFrame::kGlDefaultAlignmentBoundary);
-					cv::Mat input_frame_mat = mediapipe::formats::MatView(input_frame.get());
-					video_frame.copyTo(input_frame_mat);
-					Ref<MediaPipeImage> image = MediaPipeImage::_new(input_frame);
-#if !MEDIAPIPE_DISABLE_GPU
-					if (gpu_helper.is_valid())
-						image = gpu_helper->make_gpu_image(image);
-#endif
-					camera_helper->emit_signal("new_frame", image);
-				}
-			});
+			thread = std::thread(&CameraHelperCV::camera_thread, this);
 		}
 
 		void close() {
