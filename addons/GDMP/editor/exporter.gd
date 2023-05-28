622f622822daab5df651d06072a9358137160b8b
Jason Kuo <j20001970@gmail.com>
2023-05-28 09:06:47 +0800

    improve export plugin
    - include .task files
    - simplify add_files

--- addons/GDMP/editor/exporter.gd
+++ addons/GDMP/editor/exporter.gd
@@ -1,14 +1,13 @@
 extends EditorExportPlugin
 
 func _export_begin(features: PoolStringArray, is_debug: bool, path: String, flags: int) -> void:
-	var exports: PoolStringArray = find_files("res://", ["binarypb", "pbtxt", "tflite"])
-	for file in exports:
-		var f: File = File.new()
-		var result: int = f.open(file, File.READ)
-		if result != OK:
-			printerr("GDMP exporter: Failed to read %s: %d" % [file, result])
-			continue
-		add_file(file, f.get_buffer(f.get_len()), false)
+	var files := find_files("res://", ["binarypb", "pbtxt", "task", "tflite"])
+	for file in files:
+		var f := File.new()
+		if f.open(file, File.READ) == OK:
+			add_file(file, f.get_buffer(f.get_len()), false)
+		else:
+			printerr("GDMP: Failed to open %s: %d" % [file, f.get_error()])
 
 func _export_file(path: String, type: String, features: PoolStringArray) -> void:
 	if path.get_file() == "GDMP.gdnlib":
@@ -17,22 +16,21 @@ func _export_file(path: String, type: String, features: PoolStringArray) -> void
 
 func find_files(path: String, extenstions: PoolStringArray) -> PoolStringArray:
 	var files: PoolStringArray = []
-	var dir: Directory = Directory.new()
-	var result: int = dir.open(path)
-	if result != OK:
-		printerr("GDMP exporter: Failed to open %s: %d" % [path, result])
-	else:
+	var dir := Directory.new()
+	var result := dir.open(path)
+	if result == OK:
 		dir.list_dir_begin(true, true)
-		var filename: String
 		while true:
-			filename = dir.get_next()
-			if filename.empty():
+			var next := dir.get_next()
+			if next.empty():
 				break
-			var next: String = "%s%s" if path.ends_with("/") else "%s/%s"
-			next = next % [path, filename]
-			if dir.dir_exists(filename) and not next == "res://android":
+			next = path.plus_file(next)
+			if next.begins_with("res://android"):
+				continue
+			if dir.dir_exists(next):
 				files.append_array(find_files(next, extenstions))
-			elif filename.get_extension() in extenstions:
+			elif next.get_extension() in extenstions:
 				files.append(next)
-		dir.list_dir_end()
+	else:
+		printerr("GDMP: Failed to open %s: %d" % [path, result])
 	return files
