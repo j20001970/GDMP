785df694b94016e543bae010af1c20669177c28a
Jason Kuo <j20001970@gmail.com>
2024-12-06 10:57:12 +0800

    allow nested get/set in MediaPipeProto
    nested message fields can be accessed through "/"
    still lots of copy operations under the hood
    let's try to optimize it some time later
    
    Get example:
    ```
    proto.get("foo").get("bar").get("baz") # "test"
    proto.get("foo/bar/baz") # "test"
    ```
    
    Set example:
    ```
    var foo = proto.get("foo")
    var bar = foo.get("bar")
    bar.set("baz", "example")
    foo.set("bar", bar)
    proto.set("foo", foo)
    proto.set("foo/bar/baz", "example")
    ```

--- GDMP/proto/proto.cc
+++ GDMP/proto/proto.cc
@@ -99,8 +99,19 @@ int MediaPipeProto::get_repeated_field_size(String field_name) {
 
 Variant MediaPipeProto::get(String field_name) {
 	ERR_FAIL_COND_V(!is_initialized(), Variant());
-	const protobuf::FieldDescriptor *field = get_field_descriptor(field_name);
-	ERR_FAIL_COND_V(field == nullptr, false);
+	PoolStringArray names = field_name.split("/");
+	const protobuf::FieldDescriptor *field = get_field_descriptor(names[0]);
+	ERR_FAIL_COND_V(field == nullptr, Variant());
+	if (names.size() > 1) {
+		Ref<MediaPipeProto> proto = get_field(*message, field);
+		ERR_FAIL_COND_V(proto == nullptr, proto);
+		names.remove(0);
+		String next_names = names[0];
+		for (int i = 1; i < names.size(); i++) {
+			next_names = next_names.plus_file(names[i]);
+		}
+		return proto->get(next_names);
+	}
 	if (field->is_repeated())
 		return get_repeated_field_all(*message, field);
 	else
@@ -109,16 +120,39 @@ Variant MediaPipeProto::get(String field_name) {
 
 Variant MediaPipeProto::get_repeated(String field_name, int index) {
 	ERR_FAIL_COND_V(!is_initialized(), Variant());
-	const protobuf::FieldDescriptor *field = get_field_descriptor(field_name);
+	PoolStringArray names = field_name.split("/");
+	const protobuf::FieldDescriptor *field = get_field_descriptor(names[0]);
 	ERR_FAIL_COND_V(field == nullptr, Variant());
+	if (names.size() > 1) {
+		Ref<MediaPipeProto> proto = get_field(*message, field);
+		ERR_FAIL_COND_V(proto == nullptr, proto);
+		names.remove(0);
+		String next_names = names[0];
+		for (int i = 1; i < names.size(); i++) {
+			next_names = next_names.plus_file(names[i]);
+		}
+		return proto->get_repeated(next_names, index);
+	}
 	ERR_FAIL_COND_V(!field->is_repeated(), Variant());
 	return get_repeated_field(*message, field, index);
 }
 
 bool MediaPipeProto::set(String field_name, Variant value) {
 	ERR_FAIL_COND_V(!is_initialized(), false);
-	const protobuf::FieldDescriptor *field = get_field_descriptor(field_name);
+	PoolStringArray names = field_name.split("/");
+	const protobuf::FieldDescriptor *field = get_field_descriptor(names[0]);
 	ERR_FAIL_COND_V(field == nullptr, false);
+	if (names.size() > 1) {
+		Ref<MediaPipeProto> proto = get_field(*message, field);
+		ERR_FAIL_COND_V(proto == nullptr, false);
+		names.remove(0);
+		String next_names = names[0];
+		for (int i = 1; i < names.size(); i++) {
+			next_names = next_names.plus_file(names[i]);
+		}
+		ERR_FAIL_COND_V(!proto->set(next_names, value), false);
+		return set_field(*message, field, proto);
+	}
 	if (field->is_repeated()) {
 		ERR_PRINT("Setting repeated field is unimplemented.");
 		return false;
