05b43ac89c71bec888944974db76e002068ddab5
Jason Kuo <j20001970@gmail.com>
2025-01-31 15:33:44 +0800

    add mediapipe-models bucket to MediaPipeExternalFiles

--- addons/GDMP/MediaPipeExternalFiles.gd
+++ addons/GDMP/MediaPipeExternalFiles.gd
@@ -1,18 +1,16 @@
-@tool
 extends Node
 
-var external_url := "https://storage.googleapis.com/mediapipe-assets"
+var _gcs_url := "https://storage.googleapis.com"
+var _assets_url := _gcs_url.path_join("mediapipe-assets")
+var _models_url := _gcs_url.path_join("mediapipe-models")
 
 func _on_request_completed(request: HTTPRequest) -> void:
 	request.queue_free()
 	remove_child(request)
 
-func get_file(filename: String, generation: int = -1) -> HTTPRequest:
+func _get_file(url: String) -> HTTPRequest:
 	var request := HTTPRequest.new()
 	add_child(request)
-	var url := external_url.path_join(filename)
-	if generation > 0:
-		url = url + "?generation=%d" % generation
 	if request.request(url) == OK:
 		var callback := _on_request_completed.bind(request).unbind(4)
 		request.request_completed.connect(callback)
@@ -21,3 +19,13 @@ func get_file(filename: String, generation: int = -1) -> HTTPRequest:
 		request.queue_free()
 		request = null
 	return request
+
+func get_asset(path: String, generation: int = -1) -> HTTPRequest:
+	var url := _assets_url.path_join(path)
+	if generation > 0:
+		url = url + "?generation=%d" % generation
+	return _get_file(url)
+
+func get_model(path: String) -> HTTPRequest:
+	var url := _models_url.path_join(path)
+	return _get_file(url)
