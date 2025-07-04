1ea43ca8c781760c535c333454106462a0ae38f1
Jason Kuo <j20001970@gmail.com>
2025-07-04 19:32:52 +0800

    merge task base options into task

--- GDMP/tasks/task.cc
+++ GDMP/tasks/task.cc
@@ -2,8 +2,55 @@
 
 #include "godot_cpp/core/class_db.hpp"
 
+void MediaPipeTaskBaseOptions::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("get_model_asset_buffer"), &MediaPipeTaskBaseOptions::get_model_asset_buffer);
+	ClassDB::bind_method(D_METHOD("set_model_asset_buffer"), &MediaPipeTaskBaseOptions::set_model_asset_buffer);
+	ClassDB::bind_method(D_METHOD("get_model_asset_path"), &MediaPipeTaskBaseOptions::get_model_asset_path);
+	ClassDB::bind_method(D_METHOD("set_model_asset_path"), &MediaPipeTaskBaseOptions::set_model_asset_path);
+	ClassDB::bind_method(D_METHOD("get_delegate"), &MediaPipeTaskBaseOptions::get_delegate);
+	ClassDB::bind_method(D_METHOD("set_delegate"), &MediaPipeTaskBaseOptions::set_delegate);
+	ADD_PROPERTY(PropertyInfo(Variant::Type::PACKED_BYTE_ARRAY, "model_asset_buffer"), "set_model_asset_buffer", "get_model_asset_buffer");
+	ADD_PROPERTY(PropertyInfo(Variant::Type::STRING, "model_asset_path"), "set_model_asset_path", "get_model_asset_path");
+	ADD_PROPERTY(PropertyInfo(Variant::Type::INT, "delegate"), "set_delegate", "get_delegate");
+	BIND_ENUM_CONSTANT(DELEGATE_CPU);
+	BIND_ENUM_CONSTANT(DELEGATE_GPU);
+	BIND_ENUM_CONSTANT(DELEGATE_EDGETPU_NNAPI);
+}
+
+PackedByteArray MediaPipeTaskBaseOptions::get_model_asset_buffer() {
+	return model_asset_buffer;
+}
+
+void MediaPipeTaskBaseOptions::set_model_asset_buffer(PackedByteArray buffer) {
+	model_asset_buffer = buffer;
+}
+
+String MediaPipeTaskBaseOptions::get_model_asset_path() {
+	return model_asset_path;
+}
+
+void MediaPipeTaskBaseOptions::set_model_asset_path(const String &path) {
+	model_asset_path = path;
+}
+
+MediaPipeTaskBaseOptions::Delegate MediaPipeTaskBaseOptions::get_delegate() {
+	return Delegate(delegate);
+}
+
+void MediaPipeTaskBaseOptions::set_delegate(Delegate delegate) {
+	this->delegate = delegate;
+}
+
+std::unique_ptr<BaseOptions> MediaPipeTaskBaseOptions::get_base_options() {
+	std::unique_ptr<BaseOptions> base_options = std::make_unique<BaseOptions>();
+	if (model_asset_buffer.size())
+		base_options->model_asset_buffer = std::make_unique<std::string>((char *)model_asset_buffer.ptr(), model_asset_buffer.size());
+	base_options->model_asset_path = model_asset_path.utf8().get_data();
+	base_options->delegate = BaseOptions::Delegate(delegate);
+	return std::move(base_options);
+}
+
 void MediaPipeTask::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("initialize", "base_options"), &MediaPipeTask::initialize);
 	BIND_ENUM_CONSTANT(RUNNING_MODE_AUDIO_CLIPS)
 	BIND_ENUM_CONSTANT(RUNNING_MODE_AUDIO_STREAM)
 	BIND_ENUM_CONSTANT(RUNNING_MODE_IMAGE)
@@ -26,8 +73,4 @@ bool MediaPipeTask::register_task(const std::string &name, std::function<void()>
 void MediaPipeTask::register_tasks() {
 	for (auto func : get_register_map())
 		func.second();
-}
-
-bool MediaPipeTask::initialize(Ref<MediaPipeTaskBaseOptions> base_options) {
-	return false;
 }
