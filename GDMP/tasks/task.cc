3c2eccd2d3ca239ee7a4ed0733188cff4bed715a
Jason Kuo <j20001970@gmail.com>
2025-07-04 19:32:52 +0800

    merge task base options into task

--- GDMP/tasks/task.cc
+++ GDMP/tasks/task.cc
@@ -1,5 +1,49 @@
 #include "task.h"
 
+void MediaPipeTaskBaseOptions::_register_methods() {
+	register_method("get_model_asset_buffer", &MediaPipeTaskBaseOptions::get_model_asset_buffer);
+	register_method("set_model_asset_buffer", &MediaPipeTaskBaseOptions::set_model_asset_buffer);
+	register_method("get_model_asset_path", &MediaPipeTaskBaseOptions::get_model_asset_path);
+	register_method("set_model_asset_path", &MediaPipeTaskBaseOptions::set_model_asset_path);
+	register_method("get_delegate", &MediaPipeTaskBaseOptions::get_delegate);
+	register_method("set_delegate", &MediaPipeTaskBaseOptions::set_delegate);
+}
+
+void MediaPipeTaskBaseOptions::_init() {}
+
+PoolByteArray MediaPipeTaskBaseOptions::get_model_asset_buffer() {
+	return model_asset_buffer;
+}
+
+void MediaPipeTaskBaseOptions::set_model_asset_buffer(PoolByteArray buffer) {
+	model_asset_buffer = buffer;
+}
+
+String MediaPipeTaskBaseOptions::get_model_asset_path() {
+	return model_asset_path;
+}
+
+void MediaPipeTaskBaseOptions::set_model_asset_path(String path) {
+	model_asset_path = path;
+}
+
+int MediaPipeTaskBaseOptions::get_delegate() {
+	return delegate;
+}
+
+void MediaPipeTaskBaseOptions::set_delegate(int delegate) {
+	this->delegate = delegate;
+}
+
+std::unique_ptr<BaseOptions> MediaPipeTaskBaseOptions::get_base_options() {
+	std::unique_ptr<BaseOptions> base_options = std::make_unique<BaseOptions>();
+	if (model_asset_buffer.size())
+		base_options->model_asset_buffer = std::make_unique<std::string>((char *)model_asset_buffer.read().ptr(), model_asset_buffer.size());
+	base_options->model_asset_path = model_asset_path.utf8().get_data();
+	base_options->delegate = BaseOptions::Delegate(delegate);
+	return std::move(base_options);
+}
+
 std::map<std::string, std::function<void()>> &MediaPipeTask::get_register_map() {
 	static std::map<std::string, std::function<void()>> register_map;
 	return register_map;
@@ -15,8 +59,4 @@ bool MediaPipeTask::register_task(const std::string &name, std::function<void()>
 void MediaPipeTask::register_tasks() {
 	for (auto func : get_register_map())
 		func.second();
-}
-
-bool MediaPipeTask::initialize(Ref<MediaPipeTaskBaseOptions> base_options) {
-	return false;
 }
--- GDMP/tasks/task.h
+++ GDMP/tasks/task.h
@@ -7,16 +7,49 @@
 #include <string>
 
 #include "Godot.hpp"
-#include "Ref.hpp"
+#include "PoolArrays.hpp"
 #include "Reference.hpp"
+#include "String.hpp"
 
 #include "mediapipe/tasks/cc/audio/core/running_mode.h"
 #include "mediapipe/tasks/cc/vision/core/running_mode.h"
 
-#include "GDMP/tasks/base_options.h"
+#include "mediapipe/tasks/cc/core/base_options.h"
 
 using namespace godot;
 using namespace mediapipe::tasks;
+using namespace mediapipe::tasks::core;
+
+class MediaPipeTaskBaseOptions : public Reference {
+		GODOT_CLASS(MediaPipeTaskBaseOptions, Reference)
+
+	private:
+		PoolByteArray model_asset_buffer;
+		String model_asset_path;
+		int delegate;
+
+	public:
+		static void _register_methods();
+
+		enum Delegate {
+			DELEGATE_CPU = BaseOptions::Delegate::CPU,
+			DELEGATE_GPU = BaseOptions::Delegate::GPU,
+			DELEGATE_EDGETPU_NNAPI = BaseOptions::Delegate::EDGETPU_NNAPI,
+		};
+
+		void _init();
+
+		PoolByteArray get_model_asset_buffer();
+		void set_model_asset_buffer(PoolByteArray buffer);
+
+		String get_model_asset_path();
+		void set_model_asset_path(String path);
+
+		int get_delegate();
+		void set_delegate(int delegate);
+
+		std::unique_ptr<BaseOptions> get_base_options();
+};
 
 class MediaPipeTask : public Reference {
 	private:
@@ -38,8 +71,6 @@ class MediaPipeTask : public Reference {
 		};
 
 		static void register_tasks();
-
-		bool initialize(Ref<MediaPipeTaskBaseOptions> base_options);
 };
 
 #define GDMP_TASK_CLASS(NAME, TASK) \
