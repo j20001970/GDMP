edd75692836b96bd8675809bbad97e9f77dbd332
Jason Kuo <j20001970@gmail.com>
2025-10-06 16:48:47 +0800

    gpu helper update
    - remove ios-specific dependencies
    - add method for checking initialize

--- GDMP/gpu/gpu_helper.cc
+++ GDMP/gpu/gpu_helper.cc
@@ -5,27 +5,21 @@
 #if !MEDIAPIPE_DISABLE_GPU
 #include "mediapipe/gpu/gpu_buffer.h"
 #endif
-#ifdef __APPLE__
-#include "mediapipe/objc/util.h"
-#endif
 
 #if !MEDIAPIPE_DISABLE_GPU
 mediapipe::GpuBuffer MediaPipeGPUHelper::get_gpu_buffer(mediapipe::Image image) {
 	mediapipe::GpuBuffer gpu_buffer;
-#if MEDIAPIPE_GPU_BUFFER_USE_CV_PIXEL_BUFFER
-	gpu_buffer = image.GetGpuBuffer();
-#else
 	ERR_FAIL_COND_V(!gpu_helper.Initialized(), gpu_buffer);
 	gpu_helper.RunInGlContext([this, &image, &gpu_buffer]() -> void {
 		gpu_buffer = image.GetGpuBuffer();
 	});
-#endif
 	return gpu_buffer;
 }
 #endif
 
 void MediaPipeGPUHelper::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("initialize", "gpu_resources"), &MediaPipeGPUHelper::initialize);
+	ClassDB::bind_method(D_METHOD("is_initialized"), &MediaPipeGPUHelper::is_initialized);
 	ClassDB::bind_method(D_METHOD("make_gpu_image", "image"), &MediaPipeGPUHelper::make_gpu_image);
 	ClassDB::bind_method(D_METHOD("make_gpu_buffer_packet", "image"), &MediaPipeGPUHelper::make_gpu_buffer_packet);
 }
@@ -40,12 +34,23 @@ MediaPipeGPUHelper::MediaPipeGPUHelper(mediapipe::GpuResources *gpu_resources) {
 
 MediaPipeGPUHelper::~MediaPipeGPUHelper() = default;
 
-void MediaPipeGPUHelper::initialize(Ref<MediaPipeGPUResources> gpu_resources) {
+bool MediaPipeGPUHelper::initialize(Ref<MediaPipeGPUResources> gpu_resources) {
 #if !MEDIAPIPE_DISABLE_GPU
-	ERR_FAIL_COND(gpu_resources.is_null());
+	ERR_FAIL_COND_V(gpu_resources.is_null(), false);
 	gpu_helper.InitializeForTest(gpu_resources->get_gpu_resources().get());
+	return gpu_helper.Initialized();
 #else
 	ERR_PRINT("GPU support is disabled in this build.");
+	return false;
+#endif
+}
+
+bool MediaPipeGPUHelper::is_initialized() {
+#if !MEDIAPIPE_DISABLE_GPU
+	return gpu_helper.Initialized();
+#else
+	ERR_PRINT("GPU support is disabled in this build.");
+	return false;
 #endif
 }
 
