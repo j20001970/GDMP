54b9f2aa0290450b7dbe071e36ed0ec4d709e3b6
Jason Kuo <j20001970@gmail.com>
2025-10-18 16:28:16 +0800

    task runner update
    - use signal for async packets callback
    - expose more initialize parameters

--- GDMP/tasks/task_runner.cc
+++ GDMP/tasks/task_runner.cc
@@ -7,27 +7,47 @@
 #include "GDMP/util/packet_map.h"
 
 void MediaPipeTaskRunner::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("initialize", "config", "callback"), &MediaPipeTaskRunner::initialize, DEFVAL(nullptr));
+	ClassDB::bind_method(D_METHOD("initialize", "config", "async", "input_side_packets", "gpu_resources"),
+			&MediaPipeTaskRunner::initialize, DEFVAL(false), DEFVAL(Dictionary()), DEFVAL(nullptr));
 	ClassDB::bind_method(D_METHOD("process", "inputs"), &MediaPipeTaskRunner::process);
 	ClassDB::bind_method(D_METHOD("send", "inputs"), &MediaPipeTaskRunner::send);
 	ClassDB::bind_method(D_METHOD("close"), &MediaPipeTaskRunner::close);
 	ClassDB::bind_method(D_METHOD("restart"), &MediaPipeTaskRunner::restart);
 	ClassDB::bind_method(D_METHOD("get_graph_config"), &MediaPipeTaskRunner::get_graph_config);
+	ADD_SIGNAL(MethodInfo("error", PropertyInfo(Variant::STRING, "message")));
+	ADD_SIGNAL(MethodInfo("packets_callback", PropertyInfo(Variant::DICTIONARY, "packets")));
 }
 
-bool MediaPipeTaskRunner::initialize(Ref<MediaPipeGraphConfig> config, Callable callback) {
+bool MediaPipeTaskRunner::initialize(Ref<MediaPipeGraphConfig> config, bool async, Dictionary input_side_packets, Ref<MediaPipeGPUResources> gpu_resources) {
 	PacketsCallback packets_callback = nullptr;
-	if (callback.is_valid())
-		packets_callback = [this, callback](auto result) -> void {
+	if (async)
+		packets_callback = [this](absl::StatusOr<PacketMap> result) -> void {
 			Dictionary outputs;
 			if (result.ok())
 				outputs = util::packet_map_to_dict(result.value());
 			else
 				ERR_PRINT(result.status().message().data());
-			callback.call_deferred(outputs);
+			call_deferred("emit_signal", "packets_callback", outputs);
 		};
 	auto op_resolver = std::make_unique<MediaPipeBuiltinOpResolver>();
-	auto create_task = TaskRunner::Create(config->get_config(), std::move(op_resolver), packets_callback);
+	PacketMap side_packets = {};
+	if (input_side_packets.size() > 0)
+		side_packets = util::dict_to_packet_map(input_side_packets);
+	std::shared_ptr<mediapipe::GpuResources> gpu = nullptr;
+	if (gpu_resources.is_valid())
+#if !MEDIAPIPE_DISABLE_GPU
+		gpu = gpu_resources->get_gpu_resources();
+#else
+		ERR_PRINT("GPU support is disabled in this build.");
+#endif
+	ErrorFn error_fn = [this](absl::Status status) -> void {
+		call_deferred("emit_signal", "error", status.message().data());
+	};
+#if !MEDIAPIPE_DISABLE_GPU
+	auto create_task = TaskRunner::Create(config->get_config(), std::move(op_resolver), packets_callback, nullptr, side_packets, gpu, error_fn);
+#else
+	auto create_task = TaskRunner::Create(config->get_config(), std::move(op_resolver), packets_callback, nullptr, side_packets, error_fn);
+#endif
 	if (create_task.ok())
 		task_runner = std::move(create_task.value());
 	else
--- GDMP/tasks/task_runner.h
+++ GDMP/tasks/task_runner.h
@@ -9,6 +9,7 @@
 #include "mediapipe/tasks/cc/core/task_runner.h"
 
 #include "GDMP/framework/graph_config.h"
+#include "GDMP/gpu/gpu_resources.h"
 
 using namespace godot;
 using namespace mediapipe::tasks::core;
@@ -23,14 +24,15 @@ class MediaPipeTaskRunner : public RefCounted {
 		static void _bind_methods();
 
 	public:
-		// Initialize the task runner with graph config, optionally with callback for asynchronous result.
-		bool initialize(Ref<MediaPipeGraphConfig> config, Callable callback);
+		// Initialize the task runner with graph config.
+		// Use `async` to initialize with asynchronous mode.
+		bool initialize(Ref<MediaPipeGraphConfig> config, bool async, Dictionary input_side_packets, Ref<MediaPipeGPUResources> gpu_resources);
 
 		// Synchronous method for processing input packets.
 		// Current thread is blocked until a result is returned.
 		Dictionary process(Dictionary inputs);
 		// Asynchronous method for handling live streaming data.
-		// Packets callback must be provided for receiving output packets.
+		// Must be initialized with asynchronous mode to use this method.
 		bool send(Dictionary inputs);
 
 		// Shut down the task runner.
